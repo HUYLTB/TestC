ef08b7759d73dc9231f35247273fdf1855885309
HUYLTB <huytlb1505@gmail.com>
2022-07-10 01:05:09 +0700

    HEHE

--- src/Shape/Shape.cpp
+++ src/Shape/Shape.cpp
@@ -1,1 +1,4 @@
 #include"Shape.h"
+//Em co tao mot ham de chuyen tu string - int trong shape nhung bi loi
+//Nen em tach ra de no ben mot class khac ten la "ToInteger"
+//Goi Ham thi oke
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include"ShapeFatory/ShapeFatory.h"
 #include<string>
 #include<fstream>
+//em co lam theo cach quan li src trong dep mat va de quan li
 int main()
 {
 	ShapeFatory ShFatory;
